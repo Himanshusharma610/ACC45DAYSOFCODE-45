785f14ecd40db1c80526c573c8f43c8b4be78c12
Himanshusharma610 <himanshusharma6102006@gmail.com>
2024-09-28 13:23:34 +0530

    Add files via upload

---
+++ day 1 .com.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    int X;
+    printf("enter the score");
+    scanf("%d",&X);
+    
+    if(X >=12) {
+    printf(" yes\n");}
+    else
+{ printf("no\n");}   
+    return 0;
+}
